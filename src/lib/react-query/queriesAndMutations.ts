7198c1b2acd6b90574bee6ff701df52a005ef7ec
Bahaa Zaki <bahaa.zaki@damen.com.eg>
2024-02-28 10:25:31 +0200

    add topbar component

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -5,7 +5,7 @@ import {
 	useQueryClient,
 	useInfiniteQuery,
 } from "@tanstack/react-query";
-import { createUserAccount, signInAccount } from "../appwrite/api";
+import { createUserAccount, signInAccount, signOutAccount } from "../appwrite/api";
 
 export const useCreateAccount = () => {
 	return useMutation({
@@ -16,5 +16,11 @@ export const useCreateAccount = () => {
 export const useSignInAccount = () => {
 	return useMutation({
 		mutationFn: (user: { email: string; password: string }) => signInAccount(user),
+	});
+};
+
+export const useSignOutAccount = () => {
+	return useMutation({
+		mutationFn: signOutAccount,
 	});
 };
