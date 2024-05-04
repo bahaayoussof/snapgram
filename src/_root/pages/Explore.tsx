ec5ae4d1e56e8e0bdb4c4f9b193a6b6ad1b03817
Bahaa Youssof <bahaayoussof@gmail.com>
2024-05-04 21:46:36 +0300

    add Explore Page

--- src/_root/pages/Explore.tsx
+++ src/_root/pages/Explore.tsx
@@ -1,5 +1,81 @@
+import { useState, useEffect } from "react";
+import { Input } from "@/components/ui/input";
+import GridPostList from "@/components/shared/GridPostList";
+import SearchResults from "@/components/shared/SearchResults";
+import { useGetPosts, useSearchPosts } from "@/lib/react-query/queriesAndMutations";
+import useDebounce from "@/hooks/useDebounce";
+import Loader from "@/components/shared/Loader";
+import { useInView } from "react-intersection-observer";
+
 const Explore = () => {
-	return <div>Explore</div>;
+	const { ref, inView } = useInView();
+	const { data: posts, fetchNextPage, hasNextPage } = useGetPosts();
+
+	const [searchValue, setSearchValue] = useState("");
+	const debouncedValue = useDebounce(searchValue, 500);
+	const { data: searchedPosts, isFetching: isSearchFetching } = useSearchPosts(debouncedValue);
+
+	useEffect(() => {
+		if (inView && !searchValue) fetchNextPage();
+	}, [inView, searchValue]);
+
+	if (!posts) {
+		return (
+			<div className="flex-center w-full h-full">
+				<Loader />
+			</div>
+		);
+	}
+
+	const shouldShowSearchResults = searchValue !== "";
+	const shouldShowPosts =
+		!shouldShowSearchResults && posts.pages.every(page => page?.documents.length === 0);
+
+	return (
+		<div className="explore-container">
+			<div className="explore-inner_container">
+				<h2 className="h3-bold md:h-2-bold w-full">Search Posts</h2>
+
+				<div className="flex gap-1 px-4 w-full rounded-lg bg-dark-4">
+					<img src="/assets/icons/search.svg" height={20} width={20} alt="search" />
+					<Input
+						type="text"
+						placeholder="Search"
+						className="explore-search"
+						value={searchValue}
+						onChange={e => setSearchValue(e.target.value)}
+					/>
+				</div>
+			</div>
+
+			<div className="flex-between w-full max-w-5xl mt-16 mb-7">
+				<h2 className="body-bold md:h-3-bold">Popular Today</h2>
+
+				<div className="flex-center gap-3 bg-dark-3 rounded-xl px-4 py-2 cursor-pointer">
+					<p className="small-medium md:base-medium text-light-2">All</p>
+					<img src="/assets/icons/filter.svg" height={20} width={20} alt="filter" />
+				</div>
+			</div>
+
+			<div className="flex flex-wrap gap-9 w-full max-w-5xl">
+				{shouldShowSearchResults ? (
+					<SearchResults isSearchFetching={isSearchFetching} searchedPosts={searchedPosts} />
+				) : shouldShowPosts ? (
+					<p className="text-light-4 mt-10 text-center w-full">End of Posts</p>
+				) : (
+					posts?.pages.map((page, index) => (
+						<GridPostList key={`page-${index}`} posts={page?.documents} />
+					))
+				)}
+			</div>
+
+			{hasNextPage && !searchValue && (
+				<div ref={ref} className="mt-10">
+					<Loader />
+				</div>
+			)}
+		</div>
+	);
 };
 
 export default Explore;
